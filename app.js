ca2030d1a2adee27a26c92f402b4b0e72226c340
theertha_avnsh <theerthaavnsh@gmail.com>
2023-01-23 23:12:49 +0530

    updates

--- app.js
+++ app.js
@@ -5,80 +5,93 @@ const music = new Audio('vande.mp3');
 const songs = [
     {
         id:'1',
-        songName:` On My Way <br>
-        <div class="subtitle">Alan Walker</div>`,
+        songName:` the 1 <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/1.jpg"
     },
     {
         id:'2',
         songName:` Alan Walker-Fade <br>
-        <div class="subtitle">Alan Walker</div>`,
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/2.jpg"
     },
     // all object type 
     {
         id:"3",
-        songName: `Cartoon - On & On <br><div class="subtitle"> Daniel Levi</div>`,
+        songName: `Cartoon - On & On <br>
+        <div class="subtitle"> Taylor Swift</div>`,
         poster: "img/3.jpg",
     },
     {
         id:"4",
-        songName: `Warriyo - Mortals <br><div class="subtitle">Mortals</div>`,
+        songName: `Warriyo - Mortals <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/4.jpg",
     },
     {
         id:"5",
-        songName: `Ertugrul Gazi <br><div class="subtitle">Ertugrul</div>`,
+        songName: `Ertugrul Gazi <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/5.jpg",
     },
     {
         id:"6",
-        songName: `Electronic Music <br><div class="subtitle">Electro</div>`,
+        songName: `Electronic Music <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/6.jpg",
     },
     {
         id:"7",
-        songName: `Agar Tum Sath Ho <br><div class="subtitle">Tamashaa</div>`,
+        songName: `Agar Tum Sath Ho <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/7.jpg",
     },
     {
         id:"8",
-        songName: `Suna Hai <br><div class="subtitle">Neha Kakker</div>`,
+        songName: `Suna Hai <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/8.jpg",
     },
     {
         id:"9",
-        songName: `Dilber <br><div class="subtitle">Satyameva Jayate</div>`,
+        songName: `Dilber <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/9.jpg",
     },
     {
         id:"10",
-        songName: `Duniya <br><div class="subtitle">Luka Chuppi</div>`,
+        songName: `Duniya <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/10.jpg",
     },
     {
         id:"11",
-        songName: `Lagdi Lahore Di <br><div class="subtitle">Street Dancer 3D</div>`,
+        songName: `Lagdi Lahore Di <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/11.jpg",
     },
     {
         id:"12",
-        songName: `Putt Jatt Da <br><div class="subtitle">Putt Jatt Da</div>`,
+        songName: `Putt Jatt Da <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/12.jpg",
     },
     {
         id:"13",
-        songName: `Baarishein <br><div class="subtitle">Atif Aslam</div>`,
+        songName: `Baarishein <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/13.jpg",
     },
     {
         id:"14",
-        songName: `Vaaste <br><div class="subtitle">Dhvani Bhanushali</div>`,
+        songName: `Vaaste <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/14.jpg",
     },
     {
         id:"15",
-        songName: `Lut Gaye <br><div class="subtitle">Jubin Nautiyal</div>`,
+        songName: `Lut Gaye <br>
+        <div class="subtitle">Taylor Swift</div>`,
         poster: "img/15.jpg",
     },
 ]
